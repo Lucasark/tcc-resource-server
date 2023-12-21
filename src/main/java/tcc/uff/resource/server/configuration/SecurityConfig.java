9d17b4516fdf3ea087997ec7c9e25a0c6e50ad52
Lucas da Silva Lima <Lucassl@id.uff.br>
2023-12-20 22:09:15 -0300

    bugfix/7 UserInfo

--- src/main/java/tcc/uff/resource/server/configuration/SecurityConfig.java
+++ src/main/java/tcc/uff/resource/server/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.oauth2.jwt.SupplierJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -37,6 +38,11 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .oauth2ResourceServer(c -> c.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.debug(true);
     }
 
     @Bean
