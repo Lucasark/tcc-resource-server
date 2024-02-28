7324753b3d5715d7f6c3b6499e3ea33c7bd9fcd3
Lucas da Silva Lima <Lucassl@id.uff.br>
2024-02-27 22:33:43 -0300

    feature

--- src/main/java/tcc/uff/resource/server/model/response/entity/CourseResponse.java
+++ src/main/java/tcc/uff/resource/server/model/response/entity/CourseResponse.java
@@ -1,5 +1,6 @@
 package tcc.uff.resource.server.model.response.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,8 +8,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
+
+import static tcc.uff.resource.server.utils.Constants.DATE_TIME_PATTERN;
 
 @Getter
 @Setter
@@ -33,6 +37,7 @@ public class CourseResponse implements Serializable {
     @Builder.Default
     private Set<CurseMemberResponse> members = new HashSet<>();
 
-    private CourseLocationResponse location;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_TIME_PATTERN)
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/tcc/uff/resource/server/repository/CustomMongoOperations.java
+++ src/main/java/tcc/uff/resource/server/repository/CustomMongoOperations.java
@@ -2,6 +2,8 @@ package tcc.uff.resource.server.repository;
 
 public interface CustomMongoOperations<T> {
 
+    //REMOVER DO SET
+
     <A> void addInSet(String valueId, String key, A value);
 
     Class<T> getTypedClass();
